8c0d0bc30df6697da35c271c118643d045557b5c
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2021-11-19 12:35:35 +0300

    2

--- TestRest/src/main/java/com/example/TestRest/TestRestApplication.java
+++ TestRest/src/main/java/com/example/TestRest/TestRestApplication.java
@@ -9,6 +9,7 @@ public class TestRestApplication {
 	public static void main(String[] args) {
 
 		System.out.println("Hi");
+		System.out.println();
 		SpringApplication.run(TestRestApplication.class, args);
 	}
 
